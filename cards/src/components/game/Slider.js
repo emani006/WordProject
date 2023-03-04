05c2668ee73d9b8ce2b8c13eebcff1c4077d55ea
Eleonora Maniecka <emaniecka34@gmail.com>
2023-03-04 13:12:49 +0100

    Slider update

--- cards/src/components/game/Slider.js
+++ cards/src/components/game/Slider.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Card from './Card'
 // import CardFlipFlop from './CardFlipFlop'
-import wordlist from '../wordlist'
+// import wordlist from '../wordlist'
 // import CardClass from './CardClass'
 import CardMini from './CardMini';
 
@@ -13,28 +13,41 @@ export default function Slider(props) {
 
     // let wordsLearnedArray = [];
 
-    // const previousCard = () => {
-    //     setCurrent(current - 1);
-    // }
-    // const nextCard = () => {
-    //     setCurrent(current + 1);
-    // }
 
     const [items] = useState (props.words);
+    // console.log(items.length);
+    // console.log(items);
 
 
-    // const previousCard = () => setCurrent((prevState) => ({ current : prevState.current - 1 }));
-    // const nextCard = () => setCurrent((prevState) => ({ current : prevState.current + 1 }));
+    // const previousCard = () => setCurrent((prevState) => ( prevState - 1 ));
+    // const nextCard = () => setCurrent((prevState) => ( prevState + 1 ));
+    
+    // кнопка перехода вперед
+    const nextCard = () => {
+        if (current === items.length){
+            setCurrent(0);
+            setWordCount(0);
+        } else {
+        setCurrent((prevState) => ( prevState + 1 ));
+        }
+    };
 
+    // кнопка перехода назад
+    const previousCard = () => {
+        if (current === items.length){
+            setCurrent(0);
+            setWordCount(0);
+        } else {
+        setCurrent((prevState) => ( prevState - 1 ));
+        }
+    };
 
-    const previousCard = () => setCurrent((prevState) => ( prevState - 1 ));
-    const nextCard = () => setCurrent((prevState) => ( prevState + 1 ));
 
 
     // альтернатива, если закончился массив выводимых слов
     const loading = <div className="loading">That's all, folks! 
                         <div>
-                            To step back, use PREVIOUS CARD button
+                            To try again, press START AGAIN
                         </div>
                     </div>;
 
@@ -68,37 +81,44 @@ export default function Slider(props) {
     <div className="cardBox">
         <main>
 
+
         {/* buttons previous */}
         <div className="navig">
-            {current > 0 ? (
+            {current > 0 && current < items.length? (
                 <button onClick={previousCard}> &laquo; Previous card</button>
-                ) : (
+                ) : current === 0 ? (
                 <button className="disabled" disabled>
                     Previous card
                 </button>
-                )}
+                ) : current === items.length ? (
+                    <button onClick={previousCard} className='start-game'> START AGAIN</button>
+                    ): ''}
         </div>
 
         {/* show Card or loading text */}
         <div>
-            {items && items.length && (current < wordlist.length) > 0 ? <Card key={items.id} word={items[current]} classname={classname} buttonClassName={buttonClassName} handleWordCounter={handleWordCounter}/> : loading}
+            {items && items.length && (current < items.length) > 0 ? <Card key={items.id} word={items[current]} classname={classname} buttonClassName={buttonClassName} handleWordCounter={handleWordCounter}/> : loading}
                 <div>
-                    {items && items.length > 0 && (current < wordlist.length) ? (
+                    {items && items.length > 0 && (current < items.length) ? (
                         <div >
                             Card {current + 1} of {items.length}
-                        </div>) : ("")}
+                        </div>) : (
+                            <div> Card {items.length} of {items.length}</div>
+                            )}
                 </div>
         </div>
 
         {/* button next */}
         <div className="navig">
-            {current <= wordlist.length - 1 ? (
+            {current < items.length-1 ? (
                 <button onClick={nextCard}>Next card &raquo; </button>
-                ) : (
-                <button className="disabled" disabled>
-                    Next card
-                </button>
-                )}
+                ) : current === items.length-1? (
+                        <button onClick={nextCard} className='button-end-game'> END GAME</button>
+                    ) : ''
+                // <button className="disabled" disabled>
+                //     Next card
+                // </button>
+                }
         </div>
         </main>
     </div>
@@ -113,7 +133,7 @@ export default function Slider(props) {
             <div className=' wordsRemain'>
                 <p>Remain</p>
                 <div>
-                    {items && items.length > 0 && (current < wordlist.length) ? (
+                    {items && items.length > 0 && (current < items.length) ? (
                             <div >
                                 Cards to study: {items.length - current - 1}
                             </div>) : (<div >
